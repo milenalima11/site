0a4f7bab5ca7fb7c774cc9bbcfbd319f07caefd5
milena <157405839+milenalima11@users.noreply.github.com>
2024-01-24 14:54:35 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+window.onload = function () {
+	alert ("Cuidado com dietas milagrosas que aparecem a toda hora na mídia, geralmente elas não são equilibradas e podem causar danos ao organismo.");
+};
